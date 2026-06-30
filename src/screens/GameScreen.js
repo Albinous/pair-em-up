1a7c2851d8045342517906a819b1515324ceecb2
Albina Kadyrgali <albina200235@gmail.com>
2026-06-30 16:40:10 +0500

    feat: add score html via js

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -15,25 +15,49 @@ export class GameScreen {
     const header = createElement('header', {
       classes: ['game-header'],
     });
-
     this.backBtn = new Button({ classes: ['game-back'], text: 'Back' }).render();
 
     const title = this.createTitle();
 
     header.append(this.backBtn, title);
 
-    container.append(header);
+    const score = this.createScore();
+
+    container.append(header, score);
 
     this.bindEvents();
 
     return container;
   }
 
-  createTitle() {
+  createTitle(titleTag, className) {
     return createElement('h1', {
       classes: ['main-title'],
       text: `${this.title}`,
     });
+  }
+
+  createScore() {
+    const score = createElement('div', {
+      classes: ['game-score'],
+    });
+    const scoreTitle = createElement('h3', {
+      classes: ['game-score__title'],
+      text: 'Score: ',
+    });
+    const scoreNumber = createElement('span', {
+      classes: ['game-score__number'],
+      text: '0',
+    });
+
+    const target = createElement('span', {
+      classes: ['game-target__number'],
+      text: '/ 100',
+    });
+
+    score.append(scoreTitle, scoreNumber, target);
+
+    return score;
   }
 
   bindEvents() {
