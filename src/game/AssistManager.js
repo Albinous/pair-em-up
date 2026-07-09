2ce6ab66de70874213cef0111e8a331228d85569
Albina Kadyrgali <albina200235@gmail.com>
2026-07-09 09:42:33 +0500

    fix: hint method

--- src/game/AssistManager.js
+++ src/game/AssistManager.js
@@ -3,29 +3,30 @@ import { PairValidator } from './PairValidator';
 export class AssistManager {
   constructor(cells) {
     this.cells = cells;
-    this.moves = [];
   }
 
   hint() {
-    this.findAvailableMoves();
-    return this.moves[0];
+    const moves = this.findAvailableMoves();
+    return moves[0];
   }
 
   findAvailableMoves() {
+    const moves = [];
     for (let i = 0; i < this.cells.length; i++) {
       if (!this.cells[i].matched) {
         for (let j = i + 1; j < this.cells.length; j++) {
           if (this.cells[j].matched) continue;
           const pairType = new PairValidator(this.cells, this.cells[i], this.cells[j]).checkPair();
-          if (pairType) this.moves.push([this.cells[i], this.cells[j]]);
+          if (pairType) moves.push([this.cells[i], this.cells[j]]);
         }
       }
     }
+    return moves;
   }
 
   countOfAvailableMoves() {
-    this.findAvailableMoves();
-    const count = this.moves.length;
+    const moves = this.findAvailableMoves();
+    const count = moves.length;
     if (count > 5) {
       return '5+';
     } else {
